4421dfed61614ebadb6a96e54c144921201e2e4a
RoB Murdock <robert.f.murdock@gmail.com>
2023-05-05 13:23:54 -0400

    fixing warning

--- async/src/commonMain/kotlin/com/zegreatrob/testmints/async/TestStyleAsync.kt
+++ async/src/commonMain/kotlin/com/zegreatrob/testmints/async/TestStyleAsync.kt
@@ -2,8 +2,9 @@ package com.zegreatrob.testmints.async
 
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.test.runTest
+import kotlin.time.Duration.Companion.milliseconds
 
-fun finalTransform(timeoutMs: Long, deferred: () -> Deferred<Unit>) = runTest(dispatchTimeoutMs = timeoutMs) {
+fun finalTransform(timeoutMs: Long, deferred: () -> Deferred<Unit>) = runTest(timeout = timeoutMs.milliseconds) {
     deferred().await()
 }
 
