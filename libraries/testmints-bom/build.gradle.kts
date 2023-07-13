b2a61bc618bcc60eb385a213e9516b00c2633db6
RoB Murdock <rob@zegreatrob.com>
2023-07-13 12:29:26 -0400

    adding action processor to BOM

--- libraries/testmints-bom/build.gradle.kts
+++ libraries/testmints-bom/build.gradle.kts
@@ -10,11 +10,14 @@ dependencies {
         api(project(":async"))
         api(project(":action"))
         api(project(":action-async"))
+        api(project(":action-annotation"))
+        api(project(":action-processor"))
         api(project(":minspy"))
         api(project(":mindiff"))
         api(project(":report"))
         api(project(":mint-logs"))
         api("com.zegreatrob.testmints:mint-logs-plugin:${rootProject.version}")
+        api("com.zegreatrob.testmints:action-mint-plugin:${rootProject.version}")
     }
 }
 
