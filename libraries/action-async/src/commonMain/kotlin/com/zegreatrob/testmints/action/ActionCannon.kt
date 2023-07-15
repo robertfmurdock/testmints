b820faa932dda105ac2d45237e95d7823a22f12b
RoB Murdock <rob@zegreatrob.com>
2023-07-15 15:39:15 -0400

    adjusting action cannon to be an interface so the type can be used for stubs

--- libraries/action-async/src/commonMain/kotlin/com/zegreatrob/testmints/action/ActionCannon.kt
+++ libraries/action-async/src/commonMain/kotlin/com/zegreatrob/testmints/action/ActionCannon.kt
@@ -2,6 +2,15 @@ package com.zegreatrob.testmints.action
 
 import com.zegreatrob.testmints.action.async.SuspendAction
 
-data class ActionCannon<out D>(val dispatcher: D, val pipe: ActionPipe = ActionPipe) {
-    suspend fun <R> fire(action: SuspendAction<D, R>): R = pipe.execute(dispatcher, action)
+interface ActionCannon<out D> {
+    suspend fun <R> fire(action: SuspendAction<D, R>): R
+
+    companion object {
+        operator fun <D> invoke(dispatcher: D, pipe: ActionPipe = ActionPipe): ActionCannon<D> =
+            DispatcherPipeCannon(dispatcher, pipe)
+    }
+}
+
+data class DispatcherPipeCannon<out D>(val dispatcher: D, val pipe: ActionPipe = ActionPipe) : ActionCannon<D> {
+    override suspend fun <R> fire(action: SuspendAction<D, R>): R = pipe.execute(dispatcher, action)
 }
