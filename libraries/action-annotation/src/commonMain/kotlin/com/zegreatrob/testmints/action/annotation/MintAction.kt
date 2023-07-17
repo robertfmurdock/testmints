18121df50c0e31767d2d193aafdac562fd955cfc
RoB Murdock <rob@zegreatrob.com>
2023-07-16 22:34:47 -0400

    renaming mintaction annotation

--- libraries/action-annotation/src/commonMain/kotlin/com/zegreatrob/testmints/action/annotation/MintAction.kt
+++ libraries/action-annotation/src/commonMain/kotlin/com/zegreatrob/testmints/action/annotation/MintAction.kt
@@ -1,3 +1,6 @@
 package com.zegreatrob.testmints.action.annotation
 
-annotation class MintAction
+annotation class ActionMint
+
+@Deprecated(message = "Use ActionMint instead.", replaceWith = ReplaceWith("ActionMint"))
+typealias MintAction = ActionMint
