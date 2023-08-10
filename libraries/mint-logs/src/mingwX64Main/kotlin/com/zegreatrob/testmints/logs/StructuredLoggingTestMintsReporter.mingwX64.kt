bff5cd943c9a430fcc05760c1d69785729912d03
RoB Murdock <rob@zegreatrob.com>
2023-08-10 18:07:34 -0400

    [patch] fixing formatting issue

--- libraries/mint-logs/src/mingwX64Main/kotlin/com/zegreatrob/testmints/logs/StructuredLoggingTestMintsReporter.mingwX64.kt
+++ libraries/mint-logs/src/mingwX64Main/kotlin/com/zegreatrob/testmints/logs/StructuredLoggingTestMintsReporter.mingwX64.kt
@@ -6,7 +6,7 @@ import io.github.oshai.kotlinlogging.KLoggingEvent
 import io.github.oshai.kotlinlogging.KotlinLoggingConfiguration
 
 actual fun platformDefaults() {
-    if(KotlinLoggingConfiguration.formatter is DefaultMessageFormatter) {
+    if (KotlinLoggingConfiguration.formatter is DefaultMessageFormatter) {
         KotlinLoggingConfiguration.formatter = DefaultWithParamsFormatter()
     }
 }
