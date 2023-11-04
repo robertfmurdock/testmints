6dc2374ce11df967a7cb8a9e2e6a16373a36000d
RoB Murdock <rob@zegreatrob.com>
2023-11-04 14:15:29 -0400

    [patch] removing the added cache settings for build tasks. might slow things down but should avoid the cache problem on build server

--- convention-plugins/src/main/kotlin/com/zegreatrob/testmints/plugins/multiplatform.gradle.kts
+++ convention-plugins/src/main/kotlin/com/zegreatrob/testmints/plugins/multiplatform.gradle.kts
@@ -1,17 +1,5 @@
 package com.zegreatrob.testmints.plugins
 
-import org.jetbrains.kotlin.gradle.plugin.mpp.MetadataDependencyTransformationTask
-import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrLink
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmCachesSetup
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinPackageJsonTask
-import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
-import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
-import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeHostTest
-import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeSimulatorTest
-import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink
-
 plugins {
     kotlin("multiplatform")
     id("com.zegreatrob.testmints.plugins.reports")
@@ -37,47 +25,4 @@ dependencies {
     "commonMainApi"(platform(
         findProject(":dependency-bom") ?: "com.zegreatrob.testmints:dependency-bom"
     ))
-}
-
-tasks.withType(org.jetbrains.kotlin.gradle.targets.js.npm.PublicPackageJsonTask::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinNativeSimulatorTest::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinNpmCachesSetup::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinNativeHostTest::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinPackageJsonTask::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinJsIrLink::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(Kotlin2JsCompile::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinNativeCompile::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinNativeLink::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinJsTest::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(org.gradle.api.tasks.bundling.Jar::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(org.gradle.jvm.tasks.Jar::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(KotlinJvmTest::class).configureEach {
-    outputs.cacheIf { true }
-}
-tasks.withType(MetadataDependencyTransformationTask::class).configureEach {
-    outputs.cacheIf { true }
 }
