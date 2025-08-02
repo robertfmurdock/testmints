1dee0f44eb15b211457ab98c9615afc440580f81
RoB Murdock <rob@zegreatrob.com>
2025-08-02 14:14:34 -0400

    [minor] gradle 9

--- libraries/mint-logs/build.gradle.kts
+++ libraries/mint-logs/build.gradle.kts
@@ -10,7 +10,7 @@ kotlin {
         binaries.executable(compilation)
 
         compilation.packageJson {
-            customField("mocha", mapOf("require" to "./kotlin/testmints-mint-logs-test.mjs"))
+            customField("mocha", mapOf("require" to "./kotlin/testmints-mint-logs.mjs"))
         }
     }
 
