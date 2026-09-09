c8539b5e02d499ab00e41b42fe94b15c62b721ea
RoB Murdock <rob@zegreatrob.com>
2026-09-09 09:38:30 -0400

    [patch] removing force-upgrade for ktlint during build

--- plugins-test/build.gradle.kts
+++ plugins-test/build.gradle.kts
@@ -12,7 +12,9 @@ val serializationVersion = libs.versions.org.jetbrains.kotlinx.serialization.get
 
 allprojects {
     configurations.configureEach {
-        if (isCanBeResolved) {
+        // Ktlint runs in an isolated worker with its own Kotlin compiler dependencies.
+        // Do not overwrite that toolchain with the versions used by this build.
+        if (isCanBeResolved && name != "ktlint") {
             resolutionStrategy.force(
                 "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion",
                 "org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion",
